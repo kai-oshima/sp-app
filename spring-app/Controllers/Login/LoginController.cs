a5d4acfb43c8727bfa2ad8153ca8b0544f970c79
kai-oshima <s145cm@gmail.com>
2021-04-24 22:45:30 +0900

    add files

---
+++ spring-app/Controllers/Login/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace spring_app.Controllers.Login
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private Boolean isCorrectUser(String name, String password)
+        {
+            String testName = "test";
+            String testPass = "password";
+            if(testName == name && testPass == password)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        [HttpGet("{name}/{pass}", Name = "Login")]
+        public String ajaxLogin(String name, String pass)
+        {
+            if(isCorrectUser(name, pass))
+            {
+                return "200 OK";
+            }
+            return "500 ERROR";
+        }
+    }
+}
