17a58294e9b8a86df1c7e505cf819d1d7a89dc2b
shohei.saito <naposuke7@gmail.com>
2021-05-29 21:44:14 +0900

    アカウントのDTO作成 アカウントの取得箇所修正

--- spring-app/Controllers/RegistAccount/RegistAccountController.cs
+++ spring-app/Controllers/RegistAccount/RegistAccountController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using spring_app.Model.RegistAccount;
 
 namespace spring_app.Controllers.RegistAccount
 {
@@ -12,11 +13,14 @@ namespace spring_app.Controllers.RegistAccount
     public class RegistAccountController : ControllerBase
     {
         [HttpPost]
-        public Dictionary<String, String> AjaxRegistAccount()
+        public Dictionary<string, string> AjaxRegistAccount([FromBody] RegistAccountEnt account)
         {
-            var test = new Dictionary<String, String>();
-            test.Add("test", "test");
-            return　test;
+            var accountInfo = new Dictionary<String, String>();
+            accountInfo.Add("firstName", account.firstName);
+            accountInfo.Add("lastName", account.lastName);
+            accountInfo.Add("userId", account.userId);
+            accountInfo.Add("password", account.password);
+            return accountInfo;
         }
 
     }
--- spring-app/Model/RegistAccount/RegistAccountEnt.cs
+++ spring-app/Model/RegistAccount/RegistAccountEnt.cs
@@ -7,5 +7,9 @@ namespace spring_app.Model.RegistAccount
 {
     public class RegistAccountEnt
     {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string userId { get; set; }
+        public string password { get; set; }
     }
 }
